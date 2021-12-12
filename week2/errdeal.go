6847c5d8a2b66cd8835c8dda70b05abf8b271063
李少坤 <lishaokun@192.168.0.102>
2021-12-12 22:50:00 +0800

    chan some

--- week2/errdeal.go
+++ week2/errdeal.go
@@ -1,1 +1,25 @@
-package week2
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	xerrors "github.com/pkg/errors"
+)
+
+func init() {
+	log.SetPrefix("TRACE: ")
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+}
+
+func ExecuteSql() error {
+	return xerrors.Wrap(sql.ErrNoRows,"具体sql")
+}
+
+func main() {
+	err := ExecuteSql()
+	if xerrors.Cause(err) == sql.ErrNoRows {
+		fmt.Printf(err.Error())
+		log.Printf(err.Error())
+	}
+}
